48d73a25080dbce602dae1ea7d9440a7ff3bbdc2
RohitTalwalkar <RohitTalwalkar@users.noreply.github.com>
2024-08-30 03:45:41 +0000

    add async log printing

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,50 @@
 "use strict";
+// Using https://www.npmjs.com/package/@datastructures-js/priority-queue
+
+const {
+  MinPriorityQueue,
+} = require('@datastructures-js/priority-queue');
+
+// n = number of log sources
+// the queue is heap-based so it will take up log(n) space
+// the queue is heap-based so it will allow for insertions in log(n) time, since we're able to binary search for where to add the item
+// the queue will have the next log as the first element, so dequeue will take log(1) time
+// javascript uses dynamic arrays so the amortized runtime of growing the array used by the heap is log(1); 
+// but yes, when it needs to increase the array size, it will pause the execution
+// we could optimize that a bit by starting out with a large array for the heap
+
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
+const waitForNextLogItem = async (logSources) => {
+  const logPromises = [];
+  for (let source of logSources) {
+    if (!source.drained) {
+      logPromises.push(source.popAsync());
+    }
+  }
+  return await Promise.all(logPromises);
+}
+
+module.exports = async (logSources, printer) => {
+  // add one item from each log source to start out
+  const logQueue = new MinPriorityQueue((source) => source.date);
+  let nextLogs = await waitForNextLogItem(logSources);
+  for (let logItem of nextLogs) {
+    logQueue.enqueue(logItem);
+  }
+
+  return new Promise(async (resolve, reject) => {
+    while (!logQueue.isEmpty()) {
+      printer.print(logQueue.dequeue());
+      nextLogs = await waitForNextLogItem(logSources);
+      for (let logItem of nextLogs) {
+        if (logItem) {
+          logQueue.enqueue(logItem);
+        }
+      }
+    }
+    printer.done();
     resolve(console.log("Async sort complete."));
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,9 +1,36 @@
 "use strict";
+// Using https://www.npmjs.com/package/@datastructures-js/priority-queue
+const {
+  MinPriorityQueue,
+} = require('@datastructures-js/priority-queue');
+
+// n = number of log sources
+// the queue is heap-based so it will take up log(n) space
+// the queue is heap-based so it will allow for insertions in log(n) time, since we're able to binary search for where to add the item
+// the queue will have the next log as the first element, so dequeue will take log(1) time
+// javascript uses dynamic arrays so the amortized runtime of growing the array used by the heap is log(1); 
+// but yes, when it needs to increase the array size, it will pause the execution
+// we could optimize that a bit by starting out with a large array for the heap
 
 // Print all entries, across all of the sources, in chronological order.
-
 module.exports = (logSources, printer) => {
-  printer.print(logSources[1].pop());
-  printer.print(logSources[0].pop());
+  // add one item from each log source to start out
+  const logQueue = new MinPriorityQueue((source) => source.date);
+  for (let source of logSources) {
+    logQueue.enqueue(source.pop());
+  }
+  while (!logQueue.isEmpty()) {
+    printer.print(logQueue.dequeue());
+   // add one item from each log source if it isn't drained
+    for (let source of logSources) {
+      if (!source.drained) {
+        let nextItem = source.pop();
+        if (nextItem && nextItem.date) {
+          logQueue.enqueue(nextItem);
+        }
+      }
+    }
+  }
+  printer.done();
   return console.log("Sync sort complete.");
 };
