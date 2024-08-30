a481672ba9cfcb23432d1a1a53f4dde6224b918e
RohitTalwalkar <RohitTalwalkar@users.noreply.github.com>
2024-08-30 03:06:59 +0000

    add priority queue

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,7 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  printer.print(logSources[1].pop());
+  printer.print(logSources[0].pop());
   return console.log("Sync sort complete.");
 };
